4cb874b8f8281dd5a31ee989cf1435ca24cb0104
GET Academy <terje@getacademy.no>
2023-03-02 13:29:14 +0100

    x

--- MyContactsFullStack/Program.cs
+++ MyContactsFullStack/Program.cs
@@ -20,4 +20,21 @@ app.MapGet("/people/{id}", (Guid id) =>
 {
     return people.SingleOrDefault(p => p.Id == id);
 });
+app.MapDelete("/people/{id}", (Guid id) =>
+{
+    var person = people.SingleOrDefault(p => p.Id == id);
+    people.Remove(person);
+});
+app.MapPost("/people", (Person person) =>
+{
+    people.Add(person);
+});
+app.MapPut("/people", (Person person) =>
+{
+    var changePerson = people.SingleOrDefault(p => p.Id == person.Id);
+    changePerson.Name = person.Name;
+    changePerson.Email = person.Email;
+});
+
+
 app.Run();
